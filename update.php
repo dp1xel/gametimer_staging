463a4503cdd72f0112d9c0f541093f18120d8582
dp1xel <49376055+dp1xel@users.noreply.github.com>
2020-10-23 13:29:07 +0300

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,4 @@
 {
-  "updateurl": "https://github.com/dp1xel/gametimer/raw/main/!gtime.luac",
+  "updateurl": "https://github.com/dp1xel/gametimer_staging/raw/main/!gtime.luac",
   "latest": "3.5"
 }
